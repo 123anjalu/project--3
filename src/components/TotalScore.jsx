bab50ee5a51eee5fda406f440ee92b8774c06a10
123anjalu <98648506+123anjalu@users.noreply.github.com>
2024-08-21 22:28:39 +0530

    Add files via upload

---
+++ src/components/TotalScore.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const TotalScore = ({ score }) => {
+  return (
+    <ScoreContainer>
+      <h1> { score }</h1>
+      <p>Total Score</p>
+    </ScoreContainer>
+  );
+};
+
+export default TotalScore;
+
+const ScoreContainer = styled.div`
+
+  max-width: 200px;
+
+ 
+  text-align: center;
+
+  h1 {
+    font-size: 100px;
+    line-height: 100px;
+    padding: 0;
+    margin: 0;
+    
+  }
+
+  p {
+    font-size: 24px;
+    font-weight: 500px;
+   
+  }
+`;
